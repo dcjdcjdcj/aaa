37ecbf6f1ff3c7ba20d418ae42f4d7a81e8d3531
dcjdcjdcj <2953212307@qq.com>
2022-07-09 21:02:38 +0800

    第二次提交

---
+++ src/main/java/com/atguigu/aaa/C.java
@@ -0,0 +1,13 @@
+package com.atguigu.aaa;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class C {
+    @RequestMapping("a")
+    @ResponseBody
+    public  String  c(){
+        return "hhh";
+    }
+}
