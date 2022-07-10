a2faed2e5f6196b6c2616953306b6a5468648bd1
dcjdcjdcj <2953212307@qq.com>
2022-07-10 13:51:26 +0800

    第九次提交

--- src/main/java/com/atguigu/aaa/F.java
+++ src/main/java/com/atguigu/aaa/F.java
@@ -1,9 +1,14 @@
+package com.atguigu.aaa;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 public class F {
     @RequestMapping("f")
     @ResponseBody
     public  String  f(){
 
-       
+        System.out.println("ff");
         return "fff";
     }
 }
