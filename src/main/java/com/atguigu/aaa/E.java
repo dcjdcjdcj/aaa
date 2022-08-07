5d7846eba19e28d48c71ed9d5eb0851bfa8a5627
dcjdcjdcj <2953212307@qq.com>
2022-08-07 21:48:33 +0800

    第十二次提交

--- src/main/java/com/atguigu/aaa/E.java
+++ src/main/java/com/atguigu/aaa/E.java
@@ -3,13 +3,20 @@ package com.atguigu.aaa;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-public class E {
+public abstract  class E {
     @RequestMapping("e")
     @ResponseBody
     public  String  e(){
 
-        System.out.println("额e" );
+//        System.out.println("额e" );
+        boolean flag=true;
         System.out.println("eeeee");
+        if(flag){
+            System.out.println("flag = " + flag);
+
+        }
         return "eee";
     }
+
+
 }
