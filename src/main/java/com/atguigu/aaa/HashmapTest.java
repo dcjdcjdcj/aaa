3b9973dec325225125849b58f649c7b18dc5ee6a
dcjdcjdcj <2953212307@qq.com>
2022-11-28 14:21:37 +0800

    Revert "测试"
    This reverts commit d7db4565ab010f2788b2db85b0729fb196bbb74c.

--- src/main/java/com/atguigu/aaa/HashmapTest.java
+++ src/main/java/com/atguigu/aaa/HashmapTest.java
@@ -3,7 +3,6 @@ package com.atguigu.aaa;
 import java.util.HashMap;
 
 public class HashmapTest {
-
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("1","2");
